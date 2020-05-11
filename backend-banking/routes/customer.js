cfa4354384e1c5d23d0b1cff1596f475fd724277
nobabykill <linh1612340@gmail.com>
2020-05-11 16:36:19 +0700

    create api add customer

--- backend-banking/routes/customer.js
+++ backend-banking/routes/customer.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const customerModel = require('../models/customer.model');
 
 const router = express.Router();
 
 /* GET users listing. */
-router.put('/add', function (req, res, next) {
-  res.send('haha');
+router.post('/add', async (req, res, next) => {
+  let result;
+  try {
+    result = await customerModel.add(req.body);
+  } catch (err) {
+    console.log(err);
+    res.status(422).json({ "err": err.sqlMessage});
+    return;
+  }
+
+  const ret = {
+    customer_id: result["insertId"],
+    ...req.body
+  }
+  delete ret.hashed_password;
+  res.status(201).json(ret);
 });
 
 module.exports = router;
