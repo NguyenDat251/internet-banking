4ee7df1a9d1d9ea522dcec48bd3b83f2b4bfc8f1
nobabykill <linh1612340@gmail.com>
2020-05-12 16:49:15 +0700

    add partner routing

--- backend-banking/app.js
+++ backend-banking/app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+
 const customerRouter = require('./routes/customer');
+const partnerRouter = require('./routes/partner');
 
 const app = express();
 
@@ -17,14 +19,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/customer', customerRouter);
+app.use('/api/partner', partnerRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
