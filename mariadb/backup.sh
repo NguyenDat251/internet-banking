47a26863649f1b4a912e08a27c816df6880aacbc
nobabykill <linh1612340@gmail.com>
2020-05-17 19:02:43 +0700

    --route option backup procedure

--- mariadb/backup.sh
+++ mariadb/backup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker exec mariadb /usr/bin/mysqldump -u root --password=cantexitvim banking > backup-db.sql
+docker exec mariadb /usr/bin/mysqldump --routines -u root --password=cantexitvim banking > backup-db.sql
