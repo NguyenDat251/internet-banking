de990391aa263304cede925b45f7bc0fcde96da3
nobabykill <linh1612340@gmail.com>
2020-05-23 17:05:04 +0700

    export deposit and withdraw api

---
+++ nodejs/backend-banking/models/transaction.models.js
@@ -0,0 +1,22 @@
+const db = require('../utils/db')
+
+module.exports = {
+  add_deposit_history: entity => {
+    // entity = {
+    //   account_number: "112233445566",
+    //   amount: 100000
+    // }
+
+    entity["ts"] = Date.now() / 1000
+    return db.add(entity, 'deposit_transaction_history')
+  },
+  add_withdraw_history: entity => {
+    // entity = {
+    //   account_number: "112233445566",
+    //   amount: 100000
+    // }
+
+    entity["ts"] = Date.now() / 1000
+    return db.add(entity, 'withdraw_transaction_history')
+  }
+}
