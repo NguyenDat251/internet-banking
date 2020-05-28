07fa151305024627fdc28ab09c70cc6cd6f47e59
nobabykill <linh1612340@gmail.com>
2020-05-28 15:57:57 +0700

    add some config

--- nodejs/backend-banking/utils/config.js
+++ nodejs/backend-banking/utils/config.js
@@ -3,5 +3,8 @@ const config = {};
 config["secret_text"] = "cantexitvim";
 config["accesstoken_exp"] = 600;
 config["refreshtoken_exp"] = "30d";
+config["otp_exp"] = 600;
+config["bankmail_address"] = "linh55909167@gmail.com";
+config["bankmail_password"] = "Linh0903611";
 
 module.exports = config;
