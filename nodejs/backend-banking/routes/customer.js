9e3a56f5473f6b6cf6b9e7aa916c893f0b54e9c4
nobabykill <linh1612340@gmail.com>
2020-05-28 13:19:58 +0700

    verify transfer money request

--- nodejs/backend-banking/routes/customer.js
+++ nodejs/backend-banking/routes/customer.js
@@ -14,7 +14,8 @@ const authenLoginCustomer = async (req, res, next) => {
   try { // find username
     customerInfo = await customerModel.searchByUserName(username);
   } catch{ // unknow username
-    res.status(401).json({ "err": "invalid username" })
+    res.status(401).json({ "err": "invalid username" });
+    return;
   }
 
   customerInfo = customerInfo[0]
@@ -22,7 +23,8 @@ const authenLoginCustomer = async (req, res, next) => {
   try {
     await bcrypt.compare(plain_password, customerInfo["hashed_password"]);
   } catch{ // not match password hash
-    res.status(401).json({ "err": "invalid password" })
+    res.status(401).json({ "err": "invalid password" });
+    return;
   }
   req.headers["customerInfo"] = customerInfo;
   next();
@@ -36,7 +38,7 @@ const authenJWT = async (req, res, next) => {
   try {
     decoded = await jwt.verify(accesstoken, secret_text);
   } catch (err) {
-    res.status(401).json({ "err": err.sqlMessage });
+    res.status(401).json({ "err": err });
     return;
   }
 
@@ -53,7 +55,7 @@ router.get("/list-credit-info", authenJWT, async (req, res) => {
   try {
     creditInfo = await creditAccountModel.searchByCustomerId(customer_id);
   } catch (err) {
-    res.status(401).json({ "err": err.sqlMessage });
+    res.status(401).json({ "err": err });
     return;
   }
 
@@ -79,15 +81,52 @@ router.post("/login", authenLoginCustomer, async (req, res) => {
 
 /* POST request local transfer */
 router.post("/local-transfer", authenJWT, async (req, res) => {
+  let result;
   const customer_id = req.headers["customer_id"];
-  let customerInfo;
+
+  const from_credit_number = req.body["from_credit_number"];
+  const to_credit_number = req.body["to_credit_number"];
+  const amount = req.body["amount"];
+  const target_fullname = req.body["target_fullname"].toUpperCase();
+
+  let from_customer_info;
+  let to_customer_info;
   try {
-    customerInfo = await customerModel.searchByCustomerId(customer_id);
+    from_customer_info = await customerModel.searchByCreditNumber(from_credit_number);
   } catch (err) {
-    res.status(401).json({ "err": err.sqlMessage });
+    res.status(401).json({ "err": "invalid from_credit_number" });
+    return;
+  }
+  try {
+    to_customer_info = await customerModel.searchByCreditNumber(to_credit_number);
+  } catch (err) {
+    res.status(401).json({ "err": "invalid to_credit_number" });
+    return;
   }
 
+  from_customer_info = from_customer_info[0];
+  to_customer_info = to_customer_info[0];
 
+  // verify request
+  if (from_customer_info["customer_id"] != customer_id) {
+    res.status(401).json({ "err": "from_credit_number value and customer_id not match" });
+    return;
+  }
+
+  const fullName = to_customer_info["lastname"] + " " + to_customer_info["firstname"];
+  if (fullName.toUpperCase() != target_fullname) {
+    res.status(401).json({ "err": "target_fullname value and to_credit_number info not match" });
+    return;
+  }
+
+  result = await creditAccountModel.searchByAccountNumber(from_credit_number);
+  const credit_info = result[0];
+  if (credit_info["balance"] - amount < 50000) {
+    res.status(401).json({ "err": "balance insufficient" });
+    return;
+  }
+
+  
 
   res.status(200).json({ "msg": "otp created successfully" });
 })
