a20f4bab6158475cac2570f87a37e9764555d065
nobabykill <linh1612340@gmail.com>
2020-05-29 15:59:49 +0700

    api add transaction history

--- nodejs/backend-banking/models/transaction.models.js
+++ nodejs/backend-banking/models/transaction.models.js
@@ -3,7 +3,7 @@ const db = require('../utils/db')
 module.exports = {
   add_deposit_history: entity => {
     // entity = {
-    //   account_number: "112233445566",
+    //   credit_number: "112233445566",
     //   amount: 100000,
     //   partner_code: "abc"
     // }
@@ -13,12 +13,36 @@ module.exports = {
   },
   add_withdraw_history: entity => {
     // entity = {
-    //   account_number: "112233445566",
+    //   credit_number: "112233445566",
     //   amount: 100000
     //   partner_code: "abc"
     // }
 
     entity["ts"] = Date.now() / 1000
     return db.add(entity, 'withdraw_transaction_history')
+  },
+  add_sent_to_history: entity => {
+    // entity = {
+    //   credit_number: "112233445566",
+    //   to_credit_number: "665544332211",
+    //   amount: 100000,
+    //   message: "fuck you bro",
+    //   partner_code: "abc"
+    // }
+
+    entity["ts"] = Date.now() / 1000
+    return db.add(entity, 'sent_to_transaction_history')
+  },
+  add_receive_from_history: entity => {
+    // entity = {
+    //   credit_number: "112233445566",
+    //   from_credit_number: "665544332211",
+    //   amount: 100000,
+    //   message: "fuck you bro",
+    //   partner_code: "abc"
+    // }
+
+    entity["ts"] = Date.now() / 1000
+    return db.add(entity, 'receive_from_transaction_history')
   }
 }
