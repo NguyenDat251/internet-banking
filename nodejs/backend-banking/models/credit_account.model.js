962cfcf4192a034da5815a17853f9c02dfc4692b
nobabykill <linh1612340@gmail.com>
2020-05-30 22:25:11 +0700

    fix bug zero ahead

--- nodejs/backend-banking/models/credit_account.model.js
+++ nodejs/backend-banking/models/credit_account.model.js
@@ -4,18 +4,19 @@ module.exports = {
   add: entity => {
     // entity = {
     //   "customer_id": 1,
-    //   "account_number": "112233445566",
+    //   "credit_number": "112233445566",
     //   "balance": 100000,
     //   "status: 1"
     // }
 
-    entity["balance"] = 100000;
+    if (!entity["balance"])
+      entity["balance"] = 100000;
     entity["status"] = 1;
     return db.add(entity, 'credit_account');
   },
 
   searchByAccountNumber: creNum => db.load(`select * from credit_account where credit_number = '${creNum}'`),
   searchByCustomerId: customer_id => db.load(`select * from credit_account where customer_id = '${customer_id}'`),
-  deposit: (creNum, amount) => db.load(`call deposit(${creNum}, ${amount})`),
-  withdraw: (creNum, amount) => db.load(`call withdraw(${creNum}, ${amount})`)
+  deposit: (creNum, amount) => db.load(`call deposit("${creNum}", ${amount})`),
+  withdraw: (creNum, amount) => db.load(`call withdraw("${creNum}", ${amount})`)
 };
