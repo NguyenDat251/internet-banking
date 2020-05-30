cc26b6762fb4501a7f7ae817b808d53e46271feb
nobabykill <linh1612340@gmail.com>
2020-05-30 22:25:38 +0700

    commnet more precisely

--- nodejs/backend-banking/models/partner.model.js
+++ nodejs/backend-banking/models/partner.model.js
@@ -4,8 +4,10 @@ const randomstring = require('randomstring');
 module.exports = {
   add: entity => {
     // entity = {
-    //   "partner_code": "admin",
+    //   "partner_code": "kianto",
     //   "public_key": "xxx",
+    //   "bankname": "kiantobank",
+    //   "partner_secret": "asdad"
     // }
     entity["bank_secret"] = randomstring.generate(20);
     return db.add(entity, 'partner_api');
