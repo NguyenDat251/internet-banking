6c07ad2f25ba9465ef00fc5ab45f38fb4372a89a
Khue Doan <khuedoan98@gmail.com>
2020-06-10 11:48:05 +0700

    Add login API for employee

--- nodejs/backend-banking/routes/employee.js
+++ nodejs/backend-banking/routes/employee.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const customerModel = require('../models/customer.model');
+const employeeModel = require('../models/employee.model');
 const creditAccountModel = require('../models/credit_account.model');
 const savingAccountModel = require('../models/saving_account.model');
 const randomString = require('randomstring');
 const config = require('../utils/config');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
+
+const authenLoginEmployee = async (req, res, next) => {
+  const username = req.body["username"];
+  const plain_password = req.body["password"];
+  let result
+
+  result = await employeeModel.searchByUserName(username);
+
+  const employeeInfo = result[0]
+  if (employeeInfo === undefined) {
+    res.status(401).json({ "err": "invalid username" });
+    return;
+  }
+
+  result = await bcrypt.compare(plain_password, employeeInfo["hashed_password"]);
+  if (result === false) { // not match password hash
+    res.status(401).json({ "err": "invalid password" });
+    return;
+  }
+  req.headers["employeeInfo"] = employeeInfo;
+  next();
+}
 
 /* POST add customer */
 router.post('/add-customer', async (req, res, next) => {
@@ -54,18 +79,18 @@ router.post("/add-saving-account", async (req, res) => {
   res.status(201).json(saveAcc);
 })
 
-router.post("/login", async (req, res) => {
+router.post("/login", authenLoginEmployee, async (req, res) => {
   const secret_text = config["secret_text"];
   const accesstoken_exp = config["accesstoken_exp"];
   const refreshtoken_exp = config["refreshtoken_exp"];
 
-  const employeeInfo = req.headers["customerInfo"];
-  const customer_id = customerInfo["customer_id"];
-  const refresh_secret = customerInfo["refresh_secret"];
+  const employeeInfo = req.headers["employeeInfo"];
+  const employee_id = employeeInfo["employee_id"];
+  const refresh_secret = employeeInfo["refresh_secret"];
 
-  const accesstoken = jwt.sign({ customer_id: customer_id }, secret_text, { expiresIn: accesstoken_exp });
+  const accesstoken = jwt.sign({ employee_id: employee_id }, secret_text, { expiresIn: accesstoken_exp });
 
-  const refreshtoken = jwt.sign({ customer_id: customer_id }, refresh_secret, { expiresIn: refreshtoken_exp });
+  const refreshtoken = jwt.sign({ employee_id: employee_id }, refresh_secret, { expiresIn: refreshtoken_exp });
 
   res.status(200).json({ "access_token": accesstoken, "refresh_token": refreshtoken });
 })
