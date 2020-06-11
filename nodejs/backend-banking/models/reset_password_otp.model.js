4c1c375deb605265cbd8ecfaabafb444e1491a65
nobabykill <linh1612340@gmail.com>
2020-06-11 21:20:53 +0700

    reset password otp model

---
+++ nodejs/backend-banking/models/reset_password_otp.model.js
@@ -0,0 +1,20 @@
+const db = require('../utils/db');
+const randomstring = require('randomstring');
+
+module.exports = {
+  add: entity => {
+    // entity = {
+    //   "customer_id": 1
+    // }
+
+    entity["otp"] = randomstring.generate({
+      length: 6,
+      charset: 'numeric'
+    });
+    entity["status"] = "pending";
+    entity["ts"] = Math.floor(Date.now() / 1000);
+    return db.add(entity, 'reset_password_otp');
+  },
+  searchResetPasswordRequest: (reset_id) => db.load(`select * from reset_password_otp where reset_id = ${reset_id}`),
+  updateResetPasswordRequest: (reset_id, status) => db.load(`update reset_password_otp set status = "${status}" where reset_id = ${reset_id}`)
+};
