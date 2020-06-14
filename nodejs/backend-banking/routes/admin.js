e4ad85cca6471f28ee689f4f39dcb2defcf3c05d
Khue Doan <khuedoan98@gmail.com>
2020-06-14 15:25:50 +0700

    Add API to update employee username

--- nodejs/backend-banking/routes/admin.js
+++ nodejs/backend-banking/routes/admin.js
@@ -52,6 +52,20 @@ router.post('/delete-employee', async (req,res) => {
     res.status(201).json(result);
 })
 
+/* POST update employee username */
+router.post('/update-employee-username', async (req,res) => {
+    let result;
+
+    try {
+        result = await employeeModel.updateUsername(req.body);
+    } catch (err) {
+        res.status(422).json({ "err": err.sqlMessage });
+        return;
+    }
+
+    res.status(201).json(result);
+})
+
 /* GET request get employee list */
 router.get("/employee-list", async (req, res) => {
   const employee_list = await employeeModel.getEmployeeList;
