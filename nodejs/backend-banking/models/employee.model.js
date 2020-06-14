4a62d077f28c8521822934c611b9efff273ae165
Khue Doan <khuedoan98@gmail.com>
2020-06-14 15:28:39 +0700

    Add function to change employee password

--- nodejs/backend-banking/models/employee.model.js
+++ nodejs/backend-banking/models/employee.model.js
@@ -27,5 +27,16 @@ module.exports = {
 
     return db.load(`update employee set username = '${entity["username"]}' where employee_id = '${entity["employee_id"]}'`);
   },
+  updatePassword: entity => {
+    // entity = {
+    //   "employee_id": "1",
+    //   "password": "admin",
+    // }
+    const hash = bcrypt.hashSync(entity.password, 8);
+    entity["hashed_password"] = hash;
+    delete entity["password"];
+
+    return db.load(`update employee set hashed_password = '${entity["hashed_password"]}' where employee_id = '${entity["employee_id"]}'`);
+  },
   deleteEmployeeByID: employee_id => db.load(`delete from employee where employee_id = '${employee_id}'`)
 };
