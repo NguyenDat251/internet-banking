61f82c9dff4b1fb3fc2de2bc818cecc4560514ad
nobabykill <linh1612340@gmail.com>
2020-06-17 22:22:45 +0700

    change gmail pass

--- nodejs/backend-banking/utils/config.js
+++ nodejs/backend-banking/utils/config.js
@@ -7,7 +7,7 @@ config["refreshtoken_exp"] = "30d";
 config["otp_exp"] = 600;
 
 config["bankmail_address"] = "linh55909167@gmail.com";
-config["bankmail_password"] = "Linh0903611";
+config["bankmail_password"] = "Linh@0903611";
 
 config["local_transfer_fee"] = 2000;
 config["interbank_transfer_fee"] = 5000;
