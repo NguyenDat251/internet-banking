4f9135ca24b2c9497975f9c89e27a8951630f6ab
Thanh Lâm Nguyễn <lamnguyen11e@gmail.com>
2020-06-19 09:50:13 +0700

    change some features in change password

--- frontend/src/pages/customer/homePage/components/content/changePassword/changePassword.jsx
+++ frontend/src/pages/customer/homePage/components/content/changePassword/changePassword.jsx
@@ -1,37 +1,68 @@
 import React, { useState } from 'react';
 import Title from '../component/title/title';
-import ConfirmPassword from './components/confirmPassword/confirmPassword';
-import ConfirmOTP from './components/confirmOTP/confirmOTP';
+import TextInput from '../component/textInput/textInput';
+import ReCAPTCHA from 'react-google-recaptcha';
+import { environment } from '../../../../../../environment';
 
 const ChangePassword = () => {
-  const hinhThuc = ['Qua email'];
   const [currentPassword, setCurrentPassword] = useState();
   const [newPassword, setNewPassword] = useState();
   const [confirmPassword, setConfirmPassword] = useState();
-  const [step, setStep] = useState(1);
+
+  const [isVerified, setIsVerified] = useState(false);
+
+  const onHandleSubmit = () => {
+    
+  };
+
+  const verifyCallback = (res) => {
+    if (res) {
+      setIsVerified(true);
+    }
+  };
 
   return (
     <div className="accountInfo">
       <Title title="THÔNG TIN CÁ NHÂN" />
-      {step === 1 && (
-        <ConfirmPassword
-          setStep={setStep}
-          confirmPassword={confirmPassword}
-          setConfirmPassword={setConfirmPassword}
-          currentPassword={currentPassword}
-          setCurrentPassword={setCurrentPassword}
-          newPassword={newPassword}
-          setNewPassword={setNewPassword}
-          hinhThuc={hinhThuc}
+      <div className="mt-4">
+        <TextInput
+          title="Mật khẩu hiện tại"
+          placeholder="Nhập mật khẩu hiện tại"
+          type="password"
+          value={currentPassword}
+          onChange={(e) => setCurrentPassword(e.target.value)}
         />
-      )}
-      {step === 2 && (
-        <ConfirmOTP
-          newPassword={newPassword}
-          setStep={setStep}
-          hinhThuc={hinhThuc}
+        <TextInput
+          title="Mật khẩu mới"
+          placeholder="Nhập mật khẩu mới"
+          type="password"
+          value={newPassword}
+          onChange={(e) => setNewPassword(e.target.value)}
         />
-      )}
+        <TextInput
+          title="Mật khẩu mới"
+          placeholder="Nhập lại mật khẩu mới"
+          type="password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <div className="mt-4 row">
+          <div className="col-4"></div>
+          <ReCAPTCHA
+            className="col-8"
+            sitekey={environment.SITE_KEY}
+            onChange={verifyCallback}
+          />
+        </div>
+        <div className="mt-5 center-align">
+          <button
+            className="btn btn-success float-center"
+            type="submit"
+            onClick={onHandleSubmit}>
+            Xác nhận
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
