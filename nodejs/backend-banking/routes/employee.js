2fd02a192b2b8c66cc63516979323879939f1d0f
Khue Doan <khuedoan98@gmail.com>
2020-06-21 22:15:54 +0700

    Add API for employee to view customer transactions

--- nodejs/backend-banking/routes/employee.js
+++ nodejs/backend-banking/routes/employee.js
@@ -117,6 +117,22 @@ router.post("/deposit-customer-credit", authenJWT, async (req, res) => {
   res.status(201).json({ "msg": "deposit success" });
 })
 
+/* GET transaction history of a customer */
+router.get("/get-customer-transactions", authenJWT, async (req, res) => {
+  const customer_id = req.query.customer_id;
+  let result;
+
+  result = await creditAccountModel.searchByCustomerId(customer_id);
+  const creditInfo = result[0];
+
+  const depositHis = await customerModel.getDepositTransactionHistory(creditInfo["credit_number"]);
+  const withdrawHis = await customerModel.getWithdrawTransactionHistory(creditInfo["credit_number"]);
+  const sendtoHis = await customerModel.getSentToTransactionHistory(creditInfo["credit_number"]);
+  const receivefromHis = await customerModel.getReceiveFromTransactionHistory(creditInfo["credit_number"]);
+
+  res.status(200).json({ "deposit_history": depositHis, "withdraw_history": withdrawHis, "sendto_history": sendtoHis, "receivefrom_history": receivefromHis });
+})
+
 router.post("/login", authenLoginEmployee, async (req, res) => {
   const secret_text = config["secret_text"];
   const accesstoken_exp = config["accesstoken_exp"];
