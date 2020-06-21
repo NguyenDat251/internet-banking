04985fb7e23f8052f4d184fbb42f8ee905a121e1
Khue Doan <khuedoan98@gmail.com>
2020-06-21 14:53:57 +0700

    Update auth for admin tests

--- test/api-admin-employee-list.sh
+++ test/api-admin-employee-list.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 curl -X GET \
+  -H 'access_token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbl9pZCI6MSwiaWF0IjoxNTkyNzI0OTYyLCJleHAiOjE1OTI3MzA5NjJ9.TQKAStKk_XbFc3kNmGFP4kkXaXYzNl1LjkrGpuWn9co' \
   'http://localhost/api/admin/employee-list'
